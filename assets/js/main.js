e7065d28d23d434841ca2ec1fd110aed4018ccf1
Melanie MacDonald <melmacd94@gmail.com>
2017-10-30 16:02:00 -0400

    eleventh

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,8 @@ $(document).ready(function() {
     
     random: function(){
         //Random Number
-            randomNumber = Math.floor(Math.random()* 120)+1;
+            randomNumber = Math.floor(Math.random()* 101)+20;
+            console.log(randomNumber);
             $("#random-number").text(randomNumber);
         },
 
