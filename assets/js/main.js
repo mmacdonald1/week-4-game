007569f01e3d92baf49027e00d1a53d9d9986543
Melanie MacDonald <melmacd94@gmail.com>
2017-10-29 21:23:18 -0400

    third

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,99 +3,151 @@ var randomNumber;
 var winCount = 0;
 var loseCount = 0;
 var counter = 0;
+var cakeGame;
 
 //DOM manipulations
 var disWin = document.getElementById("dis-win");
 var disLosses = document.getElementById("dis-losses");
 
-
-
 $(document).ready(function() {
-//Random Number
-    randomNumber = Math.floor(Math.random()* 50)+1;
-    console.log(randomNumber);
-    $("#random-number").text(randomNumber);
 
-//Crystal Portion
-
-      $("#number-to-guess").text(randomNumber);
+ cakeGame = {
 
     
-      $("#cake1").on("click", function() {
+random: function(){
+    //Random Number
+        randomNumber = Math.floor(Math.random()* 50)+1;
+        console.log(randomNumber);
+        $("#random-number").text(randomNumber);
+    },
+     
+//value:function(){
+//       randomValue = Math.floor(Math.random()* 20)+1;
+//        console.log(randomValue);
+//        $("#random-number").text(randomValue);
+//},
+    
 
-        counter += 10;
-
-        $("#your-number").text(counter);
-
-        if (counter === randomNumber) {
-
-          disWin.textContent= ++winCount;
-        }
-
-        else if (counter >= randomNumber) {
-
-          disLosses.textContent= ++loseCount;
-
-        }
-      });
-          
-        
-      $("#cake2").on("click", function() {
-
-        counter += 1;
-
-        $("#your-number").text(counter);
-
-        if (counter === randomNumber) {
-
-          disWin.textContent= ++winCount;
-        }
-
-        else if (counter >= randomNumber) {
-
-          disLosses.textContent= ++loseCount;
-
-        }
-      });
-          
-        $("#cake3").on("click", function() {
-
-        counter += 5;
-
-        $("#your-number").text(counter);
-
-        if (counter === randomNumber) {
-
-          disWin.textContent= ++winCount;
-        }
-
-        else if (counter >= randomNumber) {
-
-          disLosses.textContent= ++loseCount;
-
-        }
-      });
-        
-        $("#cake4").on("click", function() {
-
-        counter += 3;
-
-        $("#your-number").text(counter);
-
-        if (counter === randomNumber) {
-
-          disWin.textContent= ++winCount;
-        }
-
-        else if (counter >= randomNumber) {
-
-          disLosses.textContent= ++loseCount;
-
-        }
-      });
+    //Crystal Portion
+play: function(){
+    
+          $("#number-to-guess").text(randomNumber);
 
 
+          $("#cake1").on("click", function() {
 
+            counter += 10;
+
+            $("#your-number").text(counter);
+
+            if (counter === randomNumber) {
+
+              disWin.textContent= ++winCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you win");
+            }
+
+            else if (counter >= randomNumber) {
+
+              disLosses.textContent= ++loseCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you lose");
+
+            }
+          });
+
+
+          $("#cake2").on("click", function() {
+
+            counter += 1;
+
+            $("#your-number").text(counter);
+
+            if (counter === randomNumber) {
+
+              disWin.textContent= ++winCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you win");
+            }
+
+            else if (counter >= randomNumber) {
+
+              disLosses.textContent= ++loseCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you lose");
+
+            }
+          });
+
+            $("#cake3").on("click", function() {
+
+            counter += 5;
+
+            $("#your-number").text(counter);
+
+            if (counter === randomNumber) {
+
+              disWin.textContent= ++winCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you win");
+            }
+
+            else if (counter >= randomNumber) {
+
+              disLosses.textContent= ++loseCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you lose");
+
+            }
+          });
+
+            $("#cake4").on("click", function() {
+
+            counter += 3;
+
+            $("#your-number").text(counter);
+
+            if (counter === randomNumber) {
+
+              disWin.textContent= ++winCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you win");
+            }
+
+            else if (counter >= randomNumber) {
+
+              disLosses.textContent= ++loseCount;
+                cakeGame.reset();
+                cakeGame.new();
+                alert("you lose");
+
+            }
+          });
+},
+        reset:function(){
+            $("#random-number").text("");
+            counter = 0;
+            $("#your-number").text(counter);
+        },
+     
+        new: function(){
+                    cakeGame.random();
+                    cakeGame.play();
+}
+  
+
+};
+
+  cakeGame.new();
+    
   });
+
 
  
